fd8bdfdc2473614de3087f6b59e800cf6393c53f
hoseinahangari <31935838+hoseinahangari@users.noreply.github.com>
2017-12-02 18:41:12 +0330

    Add files via upload

--- server.js
+++ server.js
@@ -4,9 +4,52 @@ const bot = new TeleBot('423708409:AAHD1sfmbG4PDSoFro92hqgIFoeIIyBfI2w');
 
 // On every text message
 bot.on('text', msg => {
-    let id = msg.from.id;
+    console.log(msg)
     let text = msg.text;
-    return bot.sendMessage(id, `You said: ${ text }`);
+    return msg.reply.text(`again: ${text}`);
 });
+
+bot.on('text', msg => {
+    let id = '-1001353079241';
+    return  bot.sendMessage(id, 'salam')
+})
+
+bot.on(['/rules', '/myAds'], msg => {
+
+    let replyMarkup = bot.keyboard([
+        ['/buttons', '/inlineKeyboard'],
+        ['/start', '/hide']
+    ], { resize: true });
+
+    return bot.sendMessage(msg.from.id, 'Keyboard example.', { replyMarkup });
+
+});
+// Buttons
+bot.on('/buttons', msg => {
+    
+        let replyMarkup = bot.keyboard([
+            [bot.button('contact', 'Your contact'), bot.button('location', 'Your location')],
+            ['/back', '/hide']
+        ], {resize: true});
+    
+        return bot.sendMessage(msg.from.id, 'Button example.', {replyMarkup});
+    
+    });
+// Hide keyboard
+bot.on('/hide', msg => {
+    return bot.sendMessage(
+        msg.from.id, 'Hide keyboard example. Type /back to show.', { replyMarkup: 'hide' }
+    );
+});
+
+
+bot.on('sticker', (msg) => {
+    return msg.reply.sticker('http://www.freeiconspng.com/uploads/real-brown-eye-png-10.png', { asReply: true });
+});
+
+// bot.on('channelChatCreated', (arg)=>{
+//     console.log(arg)
+// })
+
 
 bot.connect();
